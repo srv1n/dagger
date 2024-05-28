bb710ecf8343e038cd7a4f5981a618a10e480142
srv1n <sarav@gmx.com>
2024-05-28 19:12:23 +0530

    instructions on actions

--- src/lib.rs
+++ src/lib.rs
@@ -213,6 +213,7 @@ pub struct IField {
     pub name: String,
     /// The description of the field.
     pub description: Option<String>,
+    pub instructions: Option<Vec<String>>,
     /// The data type of the field.
     // pub data_type: String, // Changed to String for simplicity in this example
     /// The reference to another node's output.
